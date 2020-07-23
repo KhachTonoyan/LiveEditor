47989a16d2ca525a7a0b04a3ef92586755b3f843
Khach Tonoyan <tonoyan.khachik@gmail.com>
2020-07-23 18:10:00 +0400

    trminal commit 2

--- src/Terminal/TerminalModel.js
+++ src/Terminal/TerminalModel.js
@@ -1,26 +1,57 @@
 import File from "../Entities/File.js"
 import Folder from "../Entities/Folder.js"
 
-const root = new Folder("root",null,null,"root")
-const ch = new Folder("src",root)
-root.children = {"index.html":new File("index.html",root),"src":ch}
-ch.children = {"data":new Folder("data",{})}
+const root = new Folder("root", null, null, "root")
+const ch = new Folder("src", root)
+root.children = { "index.js": new File("index.js", root, "alert('hello world')"), "src": ch }
+ch.children = { "data": new Folder("data", {}) }
 root.children.src.children.data.parent = root.children.src
 
-class TerminalModel{
-    constructor(){
+class TerminalModel {
+    constructor() {
         this.path = root
 
         console.log(root)
     }
-    getPath = () =>{
+    getPath = () => {
         let path = this.path
         let pathName = ""
-        while(path !== null){
+        while (path !== null) {
             pathName = pathName === "" ? path.name : `${path.name}\\${pathName}`
             path = path.parent
         }
         return "localhost:" + pathName
+    }
+    changeLocation = (loc) => {
+        if (loc === "goBack") {
+            return this.goBack()
+        }
+        else if (this.path.children[loc]) {
+            this.path = this.path.children[loc]
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    goBack = () => {
+        if (this.path.parent) {
+            this.path = this.path.parent
+            return true
+        }
+        return true
+    }
+    openFile = (title) => {
+        if (this.path.children[title] && this.path.children[title].type === "file") {
+            console.log(this.path.children[title].content)
+        }
+        else return `Don't found file ${title}`
+    }
+    runFile = (title) => {
+        if (this.path.children[title] && this.path.children[title].type === "file") {
+            return eval(this.path.children[title].content)
+        }
+        else return false
     }
 }
 
--- src/Terminal/TerminalView.js
+++ src/Terminal/TerminalView.js
@@ -1,55 +1,122 @@
 const openTerminal = document.getElementById("terminalBtn")
 const terminal = document.getElementById("terminal")
+const closeTerminal = terminal.querySelector("#closeTerminal")
+const terminalContent = terminal.querySelector("#terminalContent")
 
 class TerminalView {
     constructor() {
         this.openTerminalButton = openTerminal
         this.terminal = terminal
+        this.terminalContent = terminalContent
+        this.closeTerminal = closeTerminal
+        this.activInput = null
 
         this.openTerminalButton.onclick = () => {
             this.terminal.style.display = "block"
             this.putPath(this.getPath() + ">")
+        }
+        this.terminal.onclick = () => {
+            this.activInput && this.activInput.focus()
+        }
+        this.closeTerminal.onclick = () => {
+            this.terminal.style.display = "none"
+            this.terminalContent.innerHTML = ""
         }
     }
     bindOnGetPath = (cb) => {
         this.getPath = cb
     }
 
-    putPath = (path) => {
+    putPath = (path, ok) => {
         const main = document.createElement("p")
+        main.classList.add("main")
         main.innerHTML = `
                             <p class="path">${path}</p>
-                            <p class="logP"><input class="log activLog" maxlength="50"/></p>
+                            <p class="logP"><input class="log activLog" maxlength="60"/></p>
                          `
-        this.terminal.append(main)
-        const activInput = main.querySelector(".activLog")
-        activInput.onkeyup = this.hendleInput
-        activInput.focus()
+        this.terminalContent.append(main)
+        this.activInput = main.querySelector(".activLog")
+        this.activInput.focus()
+        this.activInput.onkeyup = this.hendleInput
+        if (ok === false && this.activInput) {
+            this.activInput.classList.remove("activLog")
+            this.activInput.disabled = "disabled"
+            this.activInput = null
+        }
     }
 
-    hendleInput = ({target}) => {
-        if (event.key === "Enter"){
-            const inpVal = target.value.replace(/\s+/g, ' ').trim().toLowerCase()
-            if(inpVal !== "") this.do(inpVal)
+    hendleInput = (event) => {
+        if (event.key === "Enter") {
+            const inpVal = this.activInput.value.replace(/\s+/g, ' ').trim().toLowerCase()
+            this.activInput.classList.remove("activLog")
+            this.activInput.disabled = "disabled"
+            this.activInput.onkeyup = null
+            this.activInput = null
+            if (inpVal !== "") this.do(inpVal)
             else this.putPath(this.getPath() + ">")
-            target.classList.remove("activLog")
-            target.disabled = "disabled"
-            target.onkeyup = null
         }
     }
 
-    do = (value)=>{
+    do = (value) => {
         const commandsArray = value.split(" ")
-        if(commandsArray.length > 2){
-            this.putPath("it's don't correct command")
+        if (commandsArray.length !== 2) {
+            this.putPath("it's don't correct command", false)
             this.putPath(this.getPath() + ">")
-        } 
-        const [first,second] = commandsArray
-        console.log(first,second)
-        switch(first){
-            case "cd":
-                if(second === "..") console.log("..")
         }
+        else {
+            const [first, second] = commandsArray
+            switch (first) {
+                case "cd":
+                    {
+                        let res;
+                        if (second === "..") res = this.pathAction("goBack")
+                        else res = this.pathAction(second)
+                        if (res) this.putPath(this.getPath() + ">")
+                        else {
+                            this.putPath(`Don't found folder ${second}`, false)
+                            this.putPath(this.getPath() + ">",)
+                        }
+                    }
+                    break;
+                case "open":
+                    {
+                        let res = this.openFile(second)
+                        if (!res) this.putPath(this.getPath() + ">")
+                        else {
+                            this.putPath(res, false)
+                            this.putPath(this.getPath() + ">")
+                        }
+                    }
+                    break;
+                case "run":
+                    {
+                        let res = this.runFile(second)
+                        if (res === false) {
+                            this.putPath(`Don't found file ${second}`, res)
+                            this.putPath(this.getPath() + ">")
+                        }
+                        else {
+                            this.putPath(res, false)
+                            this.putPath(this.getPath() + ">")
+                        }
+
+                    }
+                    break;
+                default:
+                    console.log(commandsArray)
+                    this.putPath("it's don't correct command", false)
+                    this.putPath(this.getPath() + ">")
+            }
+        }
+    }
+    bindPathAction = (cb) => {
+        this.pathAction = cb
+    }
+    bindOpenFile = (cb) => {
+        this.openFile = cb
+    }
+    bindRunFile = (cb) => {
+        this.runFile = cb
     }
 }
 
